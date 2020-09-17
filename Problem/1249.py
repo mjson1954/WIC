549170c106f16f1e80eadb41eb5f39dd9d0a4916
mjson1954 <mjson1954@gmail.com>
2020-09-17 15:17:58 +0900

    20200917

--- Problem/1249.py
+++ Problem/1249.py
@@ -1,16 +1,16 @@
 from collections import deque
 
 INF = 99999
-move = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+move = [[0, 1], [1, 0], [-1, 0], [0, -1]] # 우, 하, 좌, 상
 
 def solution(queue):
-    while(len(queue)):
-        x, y = queue.popleft()
+    while(len(queue)): # 큐가 빌때까지
+        x, y = queue.popleft() 
         for i in range(len(move)):
-            dx, dy = x+move[i][0], y+move[i][1]
-            if(0 <= dx < N and 0 <= dy < N):
-                if(distance[dx][dy] > distance[x][y] + road[dx][dy]):
-                    distance[dx][dy] = distance[x][y] + road[dx][dy]
+            dx, dy = x+move[i][0], y+move[i][1] # 새로운 경로
+            if(0 <= dx < N and 0 <= dy < N): # 유효한 경로인지 검사
+                if(distance[dx][dy] > distance[x][y] + road[dx][dy]): 
+                    distance[dx][dy] = distance[x][y] + road[dx][dy] # 최소거리 갱신
                     queue.append((dx, dy))
 
 
@@ -20,7 +20,7 @@ for tc in range(1, T+1):
     road = []
     for i in range(N):
         road.append(list(map(int, input())))
-    distance = [[INF for _ in range(N)] for _ in range(N)]
+    distance = [[INF for _ in range(N)] for _ in range(N)] # 특정 좌표까지 가기 위한 최소 거리
     distance[0][0] = 0
     queue = deque()
     queue.append((0, 0))
