c15661a1e7ad1877577336c9a5966fb253ea0558
mjson1954 <mjson1954@gmail.com>
2020-10-22 13:39:46 +0900

    20201022

--- programmers/힙/디스크컨트롤러.py
+++ programmers/힙/디스크컨트롤러.py
@@ -1,26 +1,20 @@
-import heapq
-
-def solution(jobs):
-    # jobs.sort()
+def solutions(jobs):
     answer = 0
-    count = 0
-    sec = -1
-    end = 0
-    inlist = []
-    while(count < len(jobs)):
-        sec += 1
+    start = 0 # 현재까지 진행된 작업 시간
+    length = len(jobs)
+
+    jobs = sorted(jobs, key=lambda x: x[1]) # 소요시간 순대로 정렬
+
+    while(len(jobs) != 0):
         for i in range(len(jobs)):
-            if(jobs[i][0] == sec):
-                heapq.heappush(inlist, [jobs[i][1], i])
+            if(jobs[i][0] <= start):
+                start += jobs[i][1]
+                answer += start - jobs[i][0] # 현재시각(끝난시각) - 들어온시각
+                jobs.pop(i)
+                break
 
-        if(sec >= end or count == 0):
-            job, i = heapq.heappop(inlist)
-            end = job + sec # 끝나는 시각
-            count += 1
-            answer += (end - jobs[i][0])
-        
-    answer = answer / len(jobs)
-    return int(answer)
+            # 해당 시점에 아직 작업이 들어오지 않았으면 시간 ++
+            if(i==len(jobs)-1):
+                start += 1
 
-jobs = [[0,3], [1,9], [2,6]]
-print(solution(jobs))
+    return answer // length
