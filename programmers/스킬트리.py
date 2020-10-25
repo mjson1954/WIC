efe4366c0543a706aec27b1a89a490f475eb7973
mjson1954 <mjson1954@gmail.com>
2020-10-25 22:53:25 +0900

    20201025

---
+++ programmers/스킬트리.py
@@ -0,0 +1,10 @@
+def solution(skill, skill_trees):
+    answer = 0
+    
+
+    return answer
+
+
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+print(solution(skill, skill_trees))
