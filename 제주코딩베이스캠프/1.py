253b30d509cae1d2fa3d2c86a5260d1520ef7540
mjson1954 <mjson1954@gmail.com>
2021-02-09 15:51:51 +0900

    20210209

--- 제주코딩베이스캠프/1.py
+++ 제주코딩베이스캠프/1.py
@@ -1,1 +1,13 @@
-print("test")
+nums = [100, 200, 300, 400, 500]
+
+nums.remove(400)
+nums.remove(500)
+
+print(nums)
+
+
+#-----------------
+nums = [100, 200, 300, 400, 500]
+nums.pop()
+nums.pop()
+print(nums)
---
+++ 제주코딩베이스캠프/3.py
@@ -0,0 +1,9 @@
+l = [100, 200, 300]
+print(type(l))
+
+# 1)  class 'str'
+# 2)  class 'int'
+# 3)  class 'list'
+# 4)  class 'tuple'
+
+# 답 : 3번
