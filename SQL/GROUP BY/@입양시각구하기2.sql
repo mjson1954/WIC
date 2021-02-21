09ec1f224e2be0a37e9817e7783ac96aec8e386b
mjson1954 <mjson1954@gmail.com>
2021-02-21 11:53:17 +0900

    having

---
+++ SQL/GROUP BY/@입양시각구하기2.sql
@@ -0,0 +1,5 @@
+SET @HOUR = -1;
+
+SELECT @HOUR := @HOUR + 1 AS HOUR, (select count(*) from animal_outs where hour(datetime) = @hour) AS COUNT 
+FROM ANIMAL_OUTS  
+GROUP BY HOUR HAVING HOUR BETWEEN 0 AND 23
