e2216cee77b6ea0f99a7d4ab8545b4ea889aa9e6
mjson1954 <mjson1954@gmail.com>
2021-02-25 18:35:33 +0900

    20210225 dfs/bfs

--- programmers/DFS_BFS/타겟넘버.py
+++ programmers/DFS_BFS/타겟넘버.py
@@ -8,3 +8,37 @@ def solution(numbers, target):
         tree = sub_tree
 
     return tree.count(target)
+
+
+def bfs(numbers, target):
+    answer = 0
+    queue = [[numbers[0], 0], [-1*numbers[0], 0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+
+    return answer
+
+
+def dfs_solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+
+    dfs(0,0)
+    return answer
