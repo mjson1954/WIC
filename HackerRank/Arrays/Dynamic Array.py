670112209aacd274d09f6e9a89d948120486bfc8
mjson1954 <mjson1954@gmail.com>
2021-03-05 19:52:51 +0900

    20210305

--- HackerRank/Arrays/Dynamic Array.py
+++ HackerRank/Arrays/Dynamic Array.py
@@ -15,8 +15,25 @@ import sys
 #  2. 2D_INTEGER_ARRAY queries
 #
 
-# def dynamicArray(n, queries):
-    # Write your code here
+def dynamicArray(n, queries):
+    result = []
+    lastanswer = 0
+    arr = [[] for _ in range(n)]
+    for i in range(len(queries)):
+        query_type = queries[i][0]
+        x = queries[i][1]
+        y = queries[i][2]
+
+        if(query_type == 1):
+            idx = (x ^ lastanswer) % n
+            arr[idx].append(y)
+        else:
+            idx = (x ^ lastanswer) % n
+            size = len(arr[idx])
+            element = y % size
+            lastanswer = arr[idx][element]
+            result.append(lastanswer)
+    return result
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
